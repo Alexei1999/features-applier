5b37e0799e10af817913e98a38d4bc8cb2ee494d
Alexei1999 <Shulzhickij.AO.17@gmail.com>
2024-08-19 22:58:39 +0300

    fix: runs order

--- src/models/helpers/default-process-run.ts
+++ src/models/helpers/default-process-run.ts
@@ -1,68 +1,74 @@
 import { pipeline } from "../../lib/pipeline";
 import { RunConfig } from "../types/common";
 
-const splashRuns = (runsConfig: RunConfig[]): RunConfig[] => {
-  return runsConfig.reduce((runsConfig, runConfig, index) => {
-    const splashedFeatures = runConfig.appliers.reduce(
-      (appliers, applier, index) => {
-        if (index === 0) {
-          return [applier];
-        }
+const filterRuns = (runsConfig: RunConfig[]): RunConfig[] =>
+  runsConfig.filter((runConfig) => runConfig.appliers.length);
 
-        const lastApplier = appliers.at(-1);
+const splashRuns = (runsConfig: RunConfig[]): RunConfig[] =>
+  runsConfig
+    .reduce((runsConfig, runConfig, index) => {
+      if (index === 0) {
+        return [runConfig];
+      }
 
-        if (!lastApplier || lastApplier.item.name !== applier.item.name) {
-          return [...appliers, applier];
-        }
-        if (
-          lastApplier.modifiers.length !== applier.modifiers.length ||
-          applier.modifiers.some(
-            (modifier, idx) =>
-              modifier.item.name !== lastApplier.modifiers[idx].item.name
-          )
-        ) {
-          return [...appliers, applier];
-        }
+      const lastRun = runsConfig.at(-1);
 
-        return [
-          ...appliers.slice(0, -1),
-          {
-            ...lastApplier,
-            args: [...applier.args, ...lastApplier.args],
-            modifiers: [...applier.modifiers, ...lastApplier.modifiers],
-          },
-        ];
-      },
-      [] as RunConfig["appliers"]
-    );
+      if (!lastRun || lastRun.runner.name !== runConfig.runner.name) {
+        return [...runsConfig, runConfig];
+      }
 
-    const splashedRun: RunConfig = {
-      ...runConfig,
-      appliers: splashedFeatures,
-    };
+      return [
+        ...runsConfig.slice(0, -1),
+        {
+          ...lastRun,
+          appliers: [...lastRun.appliers, ...runConfig.appliers],
+        },
+      ];
+    }, [] as RunConfig[])
+    .reduce((runsConfig, runConfig) => {
+      const splashedFeatures = runConfig.appliers.reduce(
+        (appliers, applier, index) => {
+          if (index === 0) {
+            return [applier];
+          }
 
-    if (index === 0) {
-      return [splashedRun];
-    }
+          const lastApplier = appliers.at(-1);
 
-    const lastRun = runsConfig.at(-1);
+          if (!lastApplier || lastApplier.item.name !== applier.item.name) {
+            return [...appliers, applier];
+          }
+          if (
+            lastApplier.modifiers.length !== applier.modifiers.length ||
+            applier.modifiers.some(
+              (modifier, idx) =>
+                modifier.item.name !== lastApplier.modifiers[idx].item.name
+            )
+          ) {
+            return [...appliers, applier];
+          }
 
-    if (!lastRun || lastRun.runner.name !== splashedRun.runner.name) {
+          return [
+            ...appliers.slice(0, -1),
+            {
+              ...lastApplier,
+              args: [...lastApplier.args, ...applier.args],
+              modifiers: [...lastApplier.modifiers, ...applier.modifiers],
+            },
+          ];
+        },
+        [] as RunConfig["appliers"]
+      );
+
+      const splashedRun: RunConfig = {
+        ...runConfig,
+        appliers: splashedFeatures,
+      };
+
       return [...runsConfig, splashedRun];
-    }
+    }, [] as RunConfig[]);
 
-    return [
-      ...runsConfig.slice(0, -1),
-      {
-        ...lastRun,
-        appliers: [...runConfig.appliers, ...lastRun.appliers],
-      },
-    ];
-  }, [] as RunConfig[]);
-};
-
-const sortModifiers = (runsConfig: RunConfig[]): RunConfig[] => {
-  return runsConfig.map((runConfig) => ({
+const sortModifiers = (runsConfig: RunConfig[]): RunConfig[] =>
+  runsConfig.map((runConfig) => ({
     ...runConfig,
     appliers: runConfig.appliers.map((feature) => ({
       ...feature,
@@ -71,6 +77,15 @@ const sortModifiers = (runsConfig: RunConfig[]): RunConfig[] => {
       ),
     })),
   }));
-};
 
-export const defaultProcessRun = pipeline(splashRuns, sortModifiers);
+const reverseRunConfig = (runsConfig: RunConfig[]): RunConfig[] =>
+  runsConfig.map((runConfig) => ({
+    ...runConfig,
+    appliers: runConfig.appliers.toReversed(),
+  }));
+export const defaultProcessRun = pipeline(
+  filterRuns,
+  splashRuns,
+  sortModifiers,
+  reverseRunConfig
+);
